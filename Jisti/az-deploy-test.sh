de996d371f27b80bfa0d5a81f3d8e613196dc83a
Evert-Jan Lohnstein <evert-jan@live.nl>
2021-01-01 09:22:26 +0100

    test

--- Jisti/az-deploy-test.sh
+++ Jisti/az-deploy-test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -x
 source ./az-deploy.values
-
+zoneID=(az network vnet list --query [].id --output tsv)
 az network private-dns link vnet create \
     --name jisti-servers\
     --registration-enabled true \
     --resource-group ${resourcegroupname} \
-    --virtual-network $(az network vnet list --query [].id --output tsv)\ 
+    --virtual-network ${zoneID}\ 
     --zone-name ${privatednsname} \
     --subscription ${subscriptionID} \
     --tags ${tagskey1}=${tagvalue1} 
